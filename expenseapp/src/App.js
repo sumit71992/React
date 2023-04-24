7bd5f6013bffbd49f40492894d99541244dffce5
sumit71992 <sumit71992@gmail.com>
2023-04-24 09:34:51 +0530

    props added

--- expenseapp/src/App.js
+++ expenseapp/src/App.js
@@ -1,9 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expense = [
+    {
+      id: "1",
+      title: "Food",
+      amount: 10,
+      date: new Date(2023, 4, 24),
+      LocationOfExpenditure: "Delhi",
+    },
+    {
+      id: "2",
+      title: "Drama",
+      amount: 110,
+      date: new Date(2023, 3, 24),
+      LocationOfExpenditure: "Mumbai",
+    },
+    {
+      id: "3",
+      title: "Movie",
+      amount: 100,
+      date: new Date(2023, 4, 23),
+      LocationOfExpenditure: "Gurgaon",
+    },
+    {
+      id: "4",
+      title: "Petrol",
+      amount: 1000,
+      date: new Date(2023, 4, 20),
+      LocationOfExpenditure: "Gaya",
+    },
+  ];
   return (
     <div className="App">
-      <ExpenseItem />
+      {expense.map((el) => {
+        return <ExpenseItem
+          title={el.title}
+          amount={el.amount}
+          date={el.date}
+          LocationOfExpenditure={el.LocationOfExpenditure}
+        />;
+      })}
     </div>
   );
 }
--- expenseapp/src/components/ExpenseItem.js
+++ expenseapp/src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () =>{
-    const date = new Date(2023, 4, 24);
-    const title = 'Food';
-    const price = 10;
-    const LocationOfExpenditure = "Delhi";
+const ExpenseItem = (props) =>{
     return (
         <div className="expense-item">
-            <div>{date.toISOString}</div>
-            <h2>{LocationOfExpenditure}</h2>
+            <div>{props.date.toLocaleDateString()}</div>
+            <h2>{props.LocationOfExpenditure}</h2>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">
-                    ${price}
-                </div>
-                
+                    ${props.amount}
+                </div>  
             </div>
         </div>
     )
