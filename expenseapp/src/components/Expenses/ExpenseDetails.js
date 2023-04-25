6c9fc932d9ab3daad1a382e01545f2e79f4f1e74
sumit71992 <sumit71992@gmail.com>
2023-04-25 17:53:55 +0530

    useState

--- expenseapp/src/components/Expenses/ExpenseDetails.js
+++ expenseapp/src/components/Expenses/ExpenseDetails.js
@@ -1,12 +1,19 @@
+import {useState} from 'react';
+
 const ExpenseDetails = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
+  const updateTitle = ()=>{
+    setAmount(100);
+  }
   return (
     <div className="expense-item">
       <h2>{props.location}</h2>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
       </div>
-      <button className="expense-item__delete" id={props.id}>Delete Expense</button>
+      <button className="expense-item__delete" id={props.id} onClick={updateTitle}>Change Price</button>
     </div>
   );
 };
